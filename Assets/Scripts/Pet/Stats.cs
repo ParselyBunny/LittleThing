837e52b372b5a2d5de0503501bc60326fc256524
ParselyBunny <16728881+ParselyBunny@users.noreply.github.com>
2026-07-24 02:33:11 -0700

    Add new stats functionality

--- Assets/Scripts/Pet/Stats.cs
+++ Assets/Scripts/Pet/Stats.cs
@@ -1,20 +1,39 @@
 using UnityEngine;
+using System;
 
 // Handle the Pet's vital statistics
 public class Stats
 {
-	Need Hunger = new("Hunger", 100f, 100f);
-	Need Fun = new("Fun", 100f, 100f);
-	Need Love = new("Love", 100f, 100f);
+	Need _hunger = new("Hunger", 100f, 100f);
+	Need _fun = new("Fun", 100f, 100f);
+	Need _love = new("Love", 100f, 100f);
 
 	public void Update()
 	{
-		Hunger.Update(Time.deltaTime);
-		Fun.Update(Time.deltaTime);
-		Love.Update(Time.deltaTime);
+		_hunger.Update(Time.deltaTime);
+		_fun.Update(Time.deltaTime);
+		_love.Update(Time.deltaTime);
 	}
 
-	public float GetHunger() => Hunger.GetValue();
-	public float GetFun() => Fun.GetValue();
-	public float GetLove() => Love.GetValue();
+	public float GetHunger() => _hunger.GetValue();
+	public float GetFun() => _fun.GetValue();
+	public float GetLove() => _love.GetValue();
+	public void AddHunger(float val) => _hunger.Add(val);
+	public void AddHunger(Food food) => AddHunger(food.Hunger);
+	public void AddFun(float val) => _fun.Add(val);
+	public void AddFun(Food food) => AddFun(food.Fun);
+	public void AddLove(float val) => _love.Add(val);
+	public void AddLove(Food food) => AddLove(food.Love);
+	public void Resolve(Interaction interact)
+	{
+		if (interact is Food)
+		{
+			Food f = interact as Food;
+			AddHunger(f);
+		}
+		else
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
