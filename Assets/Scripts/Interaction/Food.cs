ea70b94b0e8a3ec103b7819f23ccf9348fee8749
ParselyBunny <16728881+ParselyBunny@users.noreply.github.com>
2026-08-18 22:26:41 -0700

    Reorganize scripts

--- Assets/Scripts/Interaction/Food.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Food", menuName = "Scriptable Objects/Food")]
-public class Food : Consumable
-{
-    public float Hunger = 0f;
-    public float Fun = 0f;
-    public float Love = 0f;
-}
